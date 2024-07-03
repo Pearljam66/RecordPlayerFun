4d2505513e1f123675cf942ce987f3593d7343f5
Sarah Clark <sarahireneclark@gmail.com>
2024-07-03 15:39:47 -0500

    Initial commit of project

---
+++ RecordPlayerFun/RecordPlayerFun/RecordPlayerFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordPlayerFunApp.swift
+//  RecordPlayerFun
+//
+//  Created by Sarah Clark on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct RecordPlayerFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecordPlayerFun/RecordPlayerFunTests/RecordPlayerFunTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecordPlayerFunTests.swift
+//  RecordPlayerFunTests
+//
+//  Created by Sarah Clark on 7/3/24.
+//
+
+import Testing
+@testable import RecordPlayerFun
+
+struct RecordPlayerFunTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
